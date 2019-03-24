ef5e427c987f183f16897cf4a2ff96c357d58412
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-23 22:39:45 -0500

    Add social links to header nav

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,26 +3,35 @@ import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle, siteSubtitle }) => (
-  <nav className="navbar is-primary" role="navigation">
-    <div className="container">
-      <div className="navbar-brand">
-        <h1 className="title">
-          <Link className="navbar-item" to="/">{siteTitle}</Link>
-        </h1>
-      </div>
+  <header>
+    <nav className="navbar is-primary" role="navigation">
+      <div className="container">
+        <div className="navbar-brand">
+          <h1 className="title">
+            <Link className="navbar-item" to="/">{siteTitle}</Link>
+          </h1>
+        </div>
 
-      <div className="navbar-menu">
-        <div className="navbar-start">
-          <h2 className="subtitle navbar-item">{siteSubtitle}</h2>
-        </div>
-        <div className="navbar-end">
-          <Link className="navbar-item" to="/about/">About</Link>
-          <Link className="navbar-item" to="/projects/">Projects</Link>
-          <Link className="navbar-item" to="/contact/">Contact</Link>
+        <div className="navbar-menu">
+          <div className="navbar-start">
+            <h2 className="subtitle navbar-item">{siteSubtitle}</h2>
+          </div>
+          <div className="navbar-end">
+            <Link className="navbar-item" to="/about/">About</Link>
+            <Link className="navbar-item" to="/projects/">Projects</Link>
+            <Link className="navbar-item" to="/contact/">Contact</Link>
+            <div className="navbar-item is-hoverable has-dropdown">
+              <span className="navbar-link">Social</span>
+              <div className="navbar-dropdown">
+                <Link className="navbar-item">LinkedIn</Link>
+                <Link className="navbar-item">GitHub</Link>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-  </nav>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
