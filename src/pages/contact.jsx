b0aefe6e25fa070e7fcd631304ffd211d45dfada
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-24 02:07:13 -0500

    basic form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -9,11 +9,37 @@ const ContactPage = () => (
     <SEO title="Contact Page" />
     <section className="section">
       <div className="container">
-        <h2 className="title">Contact page</h2>
-        <p>Welcome to the contact page</p>
-        <ul className="link-list">
-          <li><Link to="/">Go back to the homepage</Link></li>
-        </ul>
+        <p>Send me a message and I will reply to you promptly!</p>
+      </div>
+    </section>
+    <section className="section">
+      <div className="container">
+        <div className="field">
+          <label className="label">Name</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Full Name" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control has-icons-left has-icons-right">
+            <input className="input" type="email" placeholder="Email input" />
+            <span className="icon is-small is-left">
+              <i className="fas fa-envelope"></i>
+            </span>
+            {/* <span className="icon is-small is-right">
+              <i className="fas fa-exclamation-triangle"></i>
+            </span> */}
+          </div>
+          {/* <p className="help is-danger">This email is invalid</p> */}
+        </div>
+        <div className="field">
+          <label className="label">Message</label>
+          <textarea className="textarea" placeholder="e.g. Hello world"></textarea>
+        </div>
+        <div className="field">
+          <a className="button is-primary">Submit</a>
+        </div>
       </div>
     </section>
   </Layout>
