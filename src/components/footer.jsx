53fc8a8dfebd0d7f93bde580022f05e4edd18f8b
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-23 22:37:39 -0500

    Add nav items to footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const Footer = ({siteTitle, siteSubtitle}) => (
   <footer>
     <nav className="navbar is-black">
       <div className="container">
         <div className="navbar-menu">
-          <div className="navbar-start"></div>
+          <div className="navbar-start">
+          <Link className="navbar-item" to="/">Home</Link>
+          <Link className="navbar-item" to="/about/">About</Link>
+          <Link className="navbar-item" to="/projects/">Projects</Link>
+          <Link className="navbar-item" to="/contact/">Contact</Link>
+          </div>
           <div className="navbar-end">
             <div className="navbar-item">
               © {new Date().getFullYear()}, {siteTitle}. All rizzles for shizzle.
