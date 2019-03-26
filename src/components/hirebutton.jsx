03ef311bfe6b02d5f4eda218fe059a3e938f80f5
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-25 22:54:54 -0500

    Hire button

---
+++ src/components/hirebutton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+const HireButton = () => {
+  return (
+    <Link to="/contact/" className="button is-large is-primary">Hire Me!</Link>
+  )
+}
+
+export default HireButton
