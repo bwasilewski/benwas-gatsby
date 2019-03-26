1ebd13629173cc22ed3b1aecf0e1e761dc20d3bc
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-25 22:58:30 -0500

    Add logos and screenshots to projects

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import uniqid from 'uniqid'
+import Logo from './logo'
+import Screen from './screen'
+import _ from 'underscore'
 
-const Project = ({ title, description, timeline, tags }) => (
+
+const Project = ({ title, description, timeline, tags, logo, screen }) => (
   <section className="section">
     <h3 className="title is-3">{title}</h3>
     <div className="columns">
       <div className="column is-two-thirds">
-        {tags.length > 0 && (
+        {tags && tags.length > 0 && (
           <div className="tags">
             {tags.map(tag => (
               <span key={uniqid()} className="tag is-primary">{tag}</span>
@@ -18,7 +22,12 @@ const Project = ({ title, description, timeline, tags }) => (
         <p>{description}</p>
       </div>
       <div className="column is-one-third">
-        <p>Image here</p>
+        {!_.isUndefined(logo.childImageSharp) && !_.isNull(logo.childImageSharp) && (
+          <Logo logoPath={logo} />
+        )}
+        {!_.isUndefined(screen.childImageSharp) && !_.isNull(screen.childImageSharp) && (
+          <Screen screenPath={screen} />
+        )}
       </div>
     </div>
   </section>
@@ -29,6 +38,8 @@ Project.propTypes = {
   description: PropTypes.string,
   timeline: PropTypes.object,
   tags: PropTypes.array,
+  logo: PropTypes.object,
+  screen: PropTypes.object
 }
 
 export default Project
