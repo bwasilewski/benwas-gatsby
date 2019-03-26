bd1f5c660bff334c5b27da49af0c274e26389dec
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-25 22:59:44 -0500

    Add cards, headshot, hire buttons, testimonials

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import uniqid from 'uniqid'
 
-import Layout from "../components/layout"
-// import Image from "../components/image"
-import SEO from "../components/seo"
+import Card from '../components/card'
+import Headshot from '../components/headshot'
+import HireButton from '../components/hirebutton'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import Testimonials from '../components/testimonials'
+
 import Unsplash from '../utils/unsplash'
 
 class IndexPage extends Component {
   componentDidMount() {
-    console.log('mount')
     Unsplash()
   }
 
@@ -22,6 +25,7 @@ class IndexPage extends Component {
               siteMetadata {
                 title
                 subtitle
+                email
                 projects {
                   title
                 }
@@ -32,55 +36,56 @@ class IndexPage extends Component {
         render={data => (
           <Layout>
             <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-            <section className="hero is-medium is-primary is-bold">
+            {/* <Hero siteTitle={data.site.siteMetadata.title} siteSubtitle={data.site.siteMetadata.subtitle} /> */}
+            <section className="hero is-dark is-bold">
               <div className="hero-body">
                 <div className="container">
-                  <h1 className="title">
-                    {data.site.siteMetadata.title}
-                  </h1>
-                  <h2 className="subtitle">
-                    {data.site.siteMetadata.subtitle}
-                  </h2>
+                  <div className="columns">
+                    <div className="column is-one-third is-flex">
+                      <div style={{ width: `300px` }}>
+                        <Headshot />
+                      </div>
+                    </div>
+                    <div className="column is-flex">
+                      <div className="content is-large">
+                        <p>Hi there, thanks for stopping by! My name is <a href={`mailto:${data.site.siteMetadata.email}`}>{data.site.siteMetadata.title}</a> and I am a {data.site.siteMetadata.subtitle}. I use JavaScript and Python to build applications that run quickly and dependably.</p>
+                        <HireButton />
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </section>
-            <section className="section">
-              <div className="container">
-                <p>Hi there, thanks for stopping by! My name is {data.site.siteMetadata.title} and I am a {data.site.siteMetadata.subtitle}. I primarily work with JavaScript (React, Vue, Express, etc) and Python to build applications that run quickly and dependably.</p>
               </div>
             </section>
             <section className="section">
               <div className="container">
                 <div className="columns has-text-centered">
                   <div className="column">
-                    <div className="card">
-                      <div className="card-content">
-                        <h3 className="title is-3">
-                          <span className="icon is-large  has-text-primary">
-                            <i className="fab fa-js"></i>
-                          </span>
-                          JavaScript
-                        </h3>
-                        <p>Node, React, Redux, Express, Electron</p>
-                      </div>
-                    </div>
+                    <Card
+                      cardIcon="js"
+                      cardTitle="JavaScript"
+                      cardTags={["Node", "React", "Redux", "Express"]}
+                      cardDescription="Dolore velit do nulla consequat enim qui nisi irure irure quis dolore ut ea eiusmod. Ut et enim exercitation deserunt do eu. Ullamco eu nulla nostrud incididunt enim ut laboris. Nostrud occaecat duis excepteur sit enim." />
                   </div>
                   <div className="column">
-                    <div className="card">
-                      <div className="card-content">
-                        <h3 className="title is-3">
-                          <span className="icon is-large has-text-primary">
-                            <i className="fab fa-python"></i>
-                          </span>
-                          Python
-                        </h3>
-                        <p>Flask, Django</p>
-                      </div>
-                    </div>
+                    <Card
+                      cardIcon="python"
+                      cardTitle="Python"
+                      cardTags={["Flask", "Django"]}
+                      cardDescription="Labore excepteur sunt sint cupidatat aliqua pariatur magna minim voluptate ea Lorem laboris. In laborum eiusmod ullamco mollit laborum proident id incididunt nostrud irure ex occaecat ea eu. Adipisicing ea veniam labore culpa ullamco duis cillum." />
+                  </div>
+                  <div className="column">
+                    <Card
+                      cardIcon="wordpress-simple"
+                      cardTitle="WordPress"
+                      cardTags={["Gutenberg", "Advanced Custom Fields", "Elementor"]}
+                      cardDescription="Irure ex qui aute ipsum qui proident incididunt quis nostrud aliqua irure proident. Consectetur excepteur irure enim in deserunt eu eu cupidatat fugiat ipsum. Ipsum velit qui adipisicing reprehenderit." />
                   </div>
                 </div>
               </div>
             </section>
+
+            <Testimonials />
+
             <section className="section">
               <div className="container">
                 <h5 className="title is-5">Projects at a glance</h5>
