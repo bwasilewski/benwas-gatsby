750f300149eefa68c9b1856ba94d8db08c5819c1
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-25 22:30:13 -0500

    Adding content to config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,53 +13,141 @@ module.exports = {
           begin: 'February 2018',
           end: 'Present'
         },
-        tags: ['React', 'Redux', 'Electron']
-      }, {
+        tags: ['React', 'Redux', 'WebPack', 'Electron'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'spotmyphotos-logo-dark.png'
+      },
+      {
         title: 'Kuei Luck',
         description: 'Lorem ipsum dolor sit amet.',
         timeline: {
           begin: 'July 2018',
           end: 'March 2019'
         },
-        tags: ['WordPress', 'WPEngine']
-      }, {
+        tags: ['WordPress', 'Gulp', 'Vagrant', 'WPEngine'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'kuei_luck.svg'
+      },
+      {
+        title: 'Techgami',
+        description: 'Lorem ipsum dolor sit amet.',
+        timeline: {
+          begin: 'August 2018',
+          end: 'September 2018'
+        },
+        tags: ['WordPress', 'Gulp', 'Docker'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'techgami.svg'
+      },
+      {
+        title: 'Arcserve',
+        description: 'Lorem ipsum dolor sit amet.',
+        timeline: {
+          begin: 'August 2018',
+          end: 'September 2018'
+        },
+        tags: ['WordPress', 'Gulp', 'Vagrant', 'WPML'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'Arcserve_logo_black.png'
+      },
+      {
+        title: 'City Eye Center',
+        description: 'Lorem ipsum dolor sit amet.',
+        timeline: {
+          begin: 'July 2018',
+          end: 'August 2018'
+        },
+        tags: ['Gulp', 'Static Website'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'city_eye_center.png'
+      },
+      {
+        title: 'Grow Media',
+        description: 'Lorem ipsum dolor sit amet.',
+        timeline: {
+          begin: 'July 2018',
+          end: 'Present'
+        },
+        tags: ['WordPress', 'Gulp', 'Vagrant'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'grow-media-logo.png'
+      },
+      {
+        title: 'Bonita Beach Condo Rental',
+        description: 'Lorem ipsum dolor sit amet.',
+        timeline: {
+          begin: 'March 2016',
+          end: 'Present'
+        },
+        tags: ['Gulp', 'Static Site'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: null
+      },
+      {
+        title: 'Apervita',
+        description: 'Lorem ipsum dolor sit amet.',
+        timeline: {
+          begin: 'July 2014',
+          end: 'July 2018'
+        },
+        tags: ['Backbone', 'Django'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'apervitalogo_2c_blue_217x60.png'
+      },
+      {
+        title: 'Katherine Walker',
+        description: 'Lorem ipsum dolor sit amet.',
+        timeline: {
+          begin: 'May 2013',
+          end: 'Present'
+        },
+        tags: ['Grunt', 'WordPress'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: null
+      },
+      {
         title: 'TFW Surveying & Mapping, Inc.',
         description: 'Lorem ipsum dolor sit amet.',
         timeline: {
           begin: '2012',
           end: 'Present'
         },
-        tags: []
+        tags: ['Gulp', 'Static Website'],
+        screen: 'spotmyphotos-logo-dark.png',
+        logo: 'tfw.png'
       }
     ],
     social_media: [
-      {
-        title: 'GitHub',
-        url: 'https://github.com/bwasilewski',
-        icon: 'github-alt'
-      },
       {
         title: 'LinkedIn',
         url: 'https://www.linkedin.com/in/benwasilewski/',
         icon: 'linkedin'
       },
       {
+        title: 'GitHub',
+        url: 'https://github.com/bwasilewski',
+        icon: 'github-alt'
+      },
+      {
         title: 'Twitter',
         url: 'http://www.twitter.com/ben___was/',
         icon: 'twitter-square'
       }
+    ],
+    testimonials: [
+      {
+        quote: '...[Ben] is a creative problem-solver and effective communicator...',
+        customer: 'Erik Vorhes, VSA Partners'
+      },
+      // {
+      //   quote: 'Ben is the kind of contractor I love to hire.',
+      //   customer: 'Mattie Langenberg, VSA Partners'
+      // }
     ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -71,7 +159,28 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/misc/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/misc/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `logos`,
+        path: `${__dirname}/src/images/logos/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `screens`,
+        path: `${__dirname}/src/images/screens/`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
