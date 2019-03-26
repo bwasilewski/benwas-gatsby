cec0eaaa8b078410e4ec626949a07c6506e8bf69
Benjamin Wasilewski <ben.wasilewski@apervita.com>
2019-03-25 22:54:08 -0500

    Update styles, add hire me button

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,39 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
 import uniqid from 'uniqid'
+import Icon from './icon'
 
 const Header = ({ siteTitle, siteSubtitle, social }) => (
   <header>
-    <nav className="navbar is-transparent is-fixed-top" role="navigation">
+    <nav className="navbar is-dark is-fixed-top" role="navigation">
       <div className="container">
         <div className="navbar-brand">
-          <h1 className="title">
-            <Link className="navbar-item" to="/">{siteTitle}</Link>
-          </h1>
+          <div className="navbar-item">
+            <h1 className="title is-5">
+              <Link className="has-text-white" to="/">{siteTitle}</Link>
+            </h1>
+          </div>
         </div>
 
         <div className="navbar-menu">
           <div className="navbar-start">
-            <h2 className="subtitle navbar-item">{siteSubtitle}</h2>
+            <h2 className="subtitle is-6 navbar-item has-text-white">{siteSubtitle}</h2>
           </div>
           <div className="navbar-end">
             <Link className="navbar-item" to="/about/">About</Link>
-            <Link className="navbar-item" to="/projects/">Projects</Link>
+            <Link className="navbar-item" to="/experience/">Experience</Link>
             <Link className="navbar-item" to="/contact/">Contact</Link>
             {social.map(item => (
-              <a key={uniqid()} className="navbar-item" href={item.url}>
-                <span className="icon">
-                  <i className={`fab fa-${item.icon}`}></i>
-                </span>
-              </a>
+              <Icon key={uniqid()} iconUrl={item.url} iconCode={item.icon} />
             ))}
+            <div className="navbar-item">
+              <div className="buttons">
+                <a className="button is-primary">
+                  <strong>Hire Me</strong>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -37,12 +43,14 @@ const Header = ({ siteTitle, siteSubtitle, social }) => (
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-  siteSubtitle: PropTypes.string
+  siteSubtitle: PropTypes.string,
+  social: PropTypes.array
 }
 
 Header.defaultProps = {
   siteTitle: ``,
-  siteSubtitle: ``
+  siteSubtitle: ``,
+  social: []
 }
 
 export default Header
